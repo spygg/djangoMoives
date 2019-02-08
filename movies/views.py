01288477db01eba3112fc6bde48a934c89bd7ba7
liushi <behindmeisyou@gmail.com>
2019-02-08 15:42:14 +0800

    Add files via upload

---
+++ movies/views.py
@@ -0,0 +1,91 @@
+from django.shortcuts import render
+import os
+import re
+import mimetypes
+from wsgiref.util import FileWrapper
+
+from django.http.response import StreamingHttpResponse
+from django.conf import settings
+
+range_re = re.compile(r'bytes\s*=\s*(\d+)\s*-\s*(\d*)', re.I)
+
+
+class RangeFileWrapper(object):
+    def __init__(self, filelike, blksize=8192, offset=0, length=None):
+        self.filelike = filelike
+        self.filelike.seek(offset, os.SEEK_SET)
+        self.remaining = length
+        self.blksize = blksize
+
+    def close(self):
+        if hasattr(self.filelike, 'close'):
+            self.filelike.close()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.remaining is None:
+            # If remaining is None, we're reading the entire file.
+            data = self.filelike.read(self.blksize)
+            if data:
+                return data
+            raise StopIteration()
+        else:
+            if self.remaining <= 0:
+                raise StopIteration()
+            data = self.filelike.read(min(self.remaining, self.blksize))
+            if not data:
+                raise StopIteration()
+            self.remaining -= len(data)
+            return data
+
+
+def stream_video(request, path):
+    print(path + "##########################")
+    path = settings.MOVIE_PATH + path
+
+    range_header = request.META.get('HTTP_RANGE', '').strip()
+    range_match = range_re.match(range_header)
+    size = os.path.getsize(path)
+    content_type, encoding = mimetypes.guess_type(path)
+    content_type = content_type or 'application/octet-stream'
+    if range_match:
+        first_byte, last_byte = range_match.groups()
+        first_byte = int(first_byte) if first_byte else 0
+        last_byte = int(last_byte) if last_byte else size - 1
+        if last_byte >= size:
+            last_byte = size - 1
+        length = last_byte - first_byte + 1
+        resp = StreamingHttpResponse(RangeFileWrapper(open(
+            path, 'rb'), offset=first_byte, length=length), status=206, content_type=content_type)
+        resp['Content-Length'] = str(length)
+        resp['Content-Range'] = 'bytes %s-%s/%s' % (
+            first_byte, last_byte, size)
+    else:
+        resp = StreamingHttpResponse(FileWrapper(
+            open(path, 'rb')), content_type=content_type)
+        resp['Content-Length'] = str(size)
+    resp['Accept-Ranges'] = 'bytes'
+    return resp
+
+
+def movie(request, path):
+    return stream_video(request, path)
+
+
+def home(request):
+
+    # return HttpResponse("hello Movies")
+    context = {}
+    path = settings.MOVIE_PATH
+    filenames = os.listdir(path)
+
+    mp4list = []
+    for filename in filenames:
+        if filename.endswith('mp4') or filename.endswith('MP4'):
+            mp4list.append(filename)
+            print(filename)
+
+    context['movies'] = mp4list
+    return render(request, 'movies_list.html', context)
